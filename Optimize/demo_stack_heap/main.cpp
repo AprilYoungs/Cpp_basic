9d3b5fc4cc6b61b0c8e1f55aea180b43d5e9e24d
AprilYoungs <yangchangjun010@hotmail.com>
2018-10-07 00:12:13 +0800

    feat:cpp optimize

---
+++ Optimize/demo_stack_heap/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+int main() {
+    
+    // declare integer variables
+    int a, b, c, d, e;
+    a = 5;
+    b = 17;
+    c = 2;
+    d = 40;
+    e = 38;
+
+    // print out the addresses for each variable
+    // The ampersand symbol gives access to the address of a variable
+    std::cout << "hexadecimal addresses on the stack: \n";
+    std::cout << &a << "\n";
+    std::cout << &b << "\n";
+    std::cout << &c << "\n";
+    std::cout << &d << "\n";
+    std::cout << &e << "\n \n";
+
+    // show the addresses in decimal notation so that they are easier to interpret
+    std::cout << "same addresses in decimal: \n";
+    std::cout << (long int)&a << "\n";
+    std::cout << (long int)&b << "\n";
+    std::cout << (long int)&c << "\n";
+    std::cout << (long int)&d << "\n";
+    std::cout << (long int)&e << "\n";
+
+    // declare pointers for integer variables
+    int * f;
+    int * g;
+    int * h;
+    int * i;
+    int * j;
+    
+    /* 把变量存到 堆上,heap */
+    // new is used to create a variable on the heap. This line
+    // assigns an addresss to pointervariable and reserves enough space
+    // told hold an integer.
+    // Pointer variable holds an addresss. The address allows placing a value in
+    // memory at the address.
+    f = new int;
+    g = new int;
+    h = new int;
+    i = new int;
+    j = new int;
+    
+    // assign values to the pointers
+    *f = 5;
+    *g = 17;
+    *h = 2;
+    *i = 40;
+    *j = 38;
+    
+    // print out the hexadecimal addresses for these variables
+    std::cout << "\n hexadecimal addresses on the heap: \n";
+    std::cout << f << "\n";
+    std::cout << g << "\n";
+    std::cout << h << "\n";
+    std::cout << i << "\n";
+    std::cout << j << "\n \n";
+    
+    
+    // print out the decimal addresses for these variables
+    std::cout << "same addresses in decimal: \n";
+    
+    std::cout << (long int)f << "\n";
+    std::cout << (long int)g << "\n";
+    std::cout << (long int)h << "\n";
+    std::cout << (long int)i << "\n";
+    std::cout << (long int)j << "\n";
+
+    // remove variables from the heap
+    delete f;
+    delete g;
+    delete h;
+    delete i;
+    delete j;
+    
+    // 把指针设置为NULL,避免野指针 -》错误
+    f = NULL;
+    g = NULL;
+    h = NULL;
+    i = NULL;
+    j = NULL;
+    
+    return 0;
+}
--- SLAM/SLAM/main.cpp
+++ SLAM/SLAM/main.cpp
@@ -8,11 +8,14 @@
 
 #include "slam.hpp"
 #include "Matrix.hpp"
+#include <bitset>
 
 int main(int argc, const char * argv[]) {
-    
+
+    cout << "begin: "  << clock() << endl;
+
     vector<double> p (5,0.2);
-    
+
     vector<string> world = {"green", "red", "red", "green", "green"};
     vector<string> measurements = {"red", "green"};
     vector<double> motions = {1, 1};
@@ -21,15 +24,15 @@ int main(int argc, const char * argv[]) {
     double pExact = 0.8;
     double pOvershoot = 0.1;
     double pUndershoot = 0.1;
-    
+
     for (int k=0; k<measurements.size(); k++)
     {
         p = sense(p, measurements[k], world, pHit, pMiss);
         p = move(p, motions[k], pExact, pOvershoot, pUndershoot);
     }
-    
+
     print_vector(p);
-    
+
     /* 两种初始化对象的方法 */
     Matrix mat1({{1,2,3},{4,5,6}});
     Matrix mat2 = mat1.matrix_transpose();
@@ -39,9 +42,16 @@ int main(int argc, const char * argv[]) {
     mat2.matrix_print();
     mat4.matrix_print();
     
+//    Matrix mat10;
+    string name = "Hello!";
     
+    string *newn;
+//    memcpy(&newn, &name, name.length()+1);
+    newn = &name;
+    cout << *newn << endl;
+    name = "llal";
+    cout << *newn << endl;
+//    cout << "end: "  << clock()/double(CLOCKS_PER_SEC) << endl;
+
     return 0;
 }
-
-
-
